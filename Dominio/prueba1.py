3df2c9096452b9467b79938ebe6e5636b3227264
isma <isma@localhost.localdomain>
2025-03-07 03:41:04 +0100

    Demo primer mapa

--- Dominio/prueba1.py
+++ Dominio/prueba1.py
@@ -1,10 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='../Presentacion/static',
+            template_folder='../Presentacion/templates')
 
 @app.route('/')
 def home():
-    return send_file('../Presentacion/Prueba1.html')
+    # Renderiza el template Prueba1.html desde la carpeta "templates"
+    return render_template('Prueba1.html')
 
 if __name__ == '__main__':
+    # Escucha en todas las IPs (0.0.0.0) y puerto 5000
     app.run(host='0.0.0.0', port=5000)
