743d8390096378b0323a54dd885c8d33a0d200c4
Alejandro <115149148+AlexSaiz222@users.noreply.github.com>
2025-03-24 20:07:22 +0100

    add ocupacion_stats
    Tasa de ocupación
    Reservas confirmadas
    Cancelaciones

--- Persistencia/DAOS/OcupacionHoteleraDAO.py
+++ Persistencia/DAOS/OcupacionHoteleraDAO.py
@@ -39,3 +39,23 @@ class OcupacionHoteleraDAO:
         if result and len(result) > 0:
             return result[0].get('totalReservas', 0)
         return 0
+    
+    @staticmethod
+    def total_reservas_confirmadas():
+        pipeline = [
+            {"$group": {"_id": None, "totalReservas": {"$sum": "$reservas_confirmadas"}}}
+        ]
+        result = list(mongoDBAgent.db[OcupacionHoteleraDAO.COLLECTION].aggregate(pipeline))
+        if result and len(result) > 0:
+            return result[0].get("totalReservas", 0)
+        return 0
+
+    @staticmethod
+    def total_cancelaciones():
+        pipeline = [
+            {"$group": {"_id": None, "totalCancelaciones": {"$sum": "$cancelaciones"}}}
+        ]
+        result = list(mongoDBAgent.db[OcupacionHoteleraDAO.COLLECTION].aggregate(pipeline))
+        if result and len(result) > 0:
+            return result[0].get("totalCancelaciones", 0)
+        return 0
